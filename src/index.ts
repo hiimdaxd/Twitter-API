7279ec7c610aff5c1453c39c6efd81e374fef046
duyanhhh1811 <duyanhlucas302@gmail.com>
2024-07-15 23:56:57 +0700

    Add user route, controller and middleware for authentication

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-const username: string = 'npm run dev'
-console.log(username)
+import express from 'express'
+import usersRouter from '~/routes/users.route'
+import databaseService from './services/database.services'
+const app = express()
+const port = 3000
+
+app.use(express.json()) // Parse JSON to JavaScript object
+app.use('/users/', usersRouter)
+databaseService.connect() // Connect to DB
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
---
+++ src/routes/users.route.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+import { loginValidator } from '~/middlewares/users.middlewares'
+import { loginController } from '~/controllers/users.controller'
+
+const usersRouter = Router()
+
+usersRouter.post('/login', loginValidator, loginController)
+
+export default usersRouter
