5a57b3f765b188c4e3338a2e5d7f963427ec6792
duyanhhh1811 <duyanhlucas302@gmail.com>
2024-07-18 23:58:37 +0700

    Add user schema and database class

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,9 @@ const app = express()
 const port = 3000
 
 app.use(express.json()) // Parse JSON to JavaScript object
-app.use('/users/', usersRouter)
 databaseService.connect() // Connect to DB
+app.use('/users/', usersRouter)
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -1,5 +1,6 @@
-import { MongoClient } from 'mongodb'
+import { Collection, Db, MongoClient } from 'mongodb'
 import { config } from 'dotenv'
+import User from '~/models/schemas/User.schema'
 
 config()
 
@@ -7,20 +8,26 @@ const uri = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABA
 
 class DatabaseService {
   private client: MongoClient
+  private db: Db
 
   constructor() {
     this.client = new MongoClient(uri)
+    this.db = this.client.db(process.env.DATABASE_NAME)
   }
 
   async connect() {
     try {
       // Send a ping to confirm a successful connection
-      await this.client.db('admin').command({ ping: 1 })
+      await this.db.command({ ping: 1 })
       console.log('Pinged your deployment. You successfully connected to MongoDB!')
     } finally {
       // Ensures that the client will close when you finish/error
-      await this.client.close()
+      // await this.client.close()
     }
+  }
+
+  get getUsers(): Collection<User> {
+    return this.db.collection(process.env.DATABASE_USERS_COLLECTION as string)
   }
 }
 
